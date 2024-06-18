5b6b39607ad0547931e5433a49b16aac8a664b33
Pranay310 <146696037+Pranay310@users.noreply.github.com>
2024-06-18 19:14:29 +0530

    Building-Phase

---
+++ CodePen/src/component/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+// import Editor from 
+
+function App() {
+
+
+  return (
+    <>
+    <div className='pane top-pane'></div>
+    <div></div>
+    </>
+  )
+}
+
+export default App
